3fef78a5cf6ce908b225e4b5f91ecbd1d828252f
david-vynn <davidalvinmaulana@gmail.com>
2026-06-14 12:07:05 +0700

    Belajar DOM Manipulation Element

---
+++ DOM Manipulation part 1/script.js
@@ -0,0 +1,16 @@
+// const judul =document.getElementById('judul');
+// judul.style.color = "lightblue";
+
+// judul.innerHTML = "<em>David Alvin Maulana</em>";
+
+// const a = document.querySelector('section#a');
+// a.innerHTML = "Hello World";
+
+// const judul = document.getElementsByTagName('h1')[0];
+
+// judul.setAttribute('name', 'Leonard')
+
+// const a = document.querySelector('section#a a');
+// a.setAttribute('id','cihuy');
+
+const p2 = document.querySelector('.p2');
