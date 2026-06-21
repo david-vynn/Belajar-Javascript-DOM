960a9a91f9de1eea54c5d40e200cdb6aae633328
david-vynn <davidalvinmaulana@gmail.com>
2026-06-21 15:26:00 +0700

    todolist

---
+++ Todo sendiri/script.js
@@ -0,0 +1,12 @@
+// MEnangkap
+
+const { createElement } = require("react");
+
+const inputList = document.getElementById('inputList');
+const addBtn = document.getElementById('addBtn');
+
+list = []
+
+addBtn.addEventListener('click', function() {
+    const li = document.createElement('li', inputList)
+});
