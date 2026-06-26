916b8d418f793ea130cb87d6b8548eec3e08adcd
david-vynn <davidalvinmaulana@gmail.com>
2026-06-26 09:46:26 +0700

    Latihan array methods part 1

---
+++ latihan-array-methods/script.js
@@ -0,0 +1,93 @@
+// // Misi 1 : forEach()
+
+// const books = [
+//     'Clean Code',
+//     'Atomic habits',
+//     'Deep Work'
+// ]
+
+// books.forEach(function(element) {
+//     console.log(`Buku: ${element}`)
+// });
+
+// const books = [
+//     'Clean Code',
+//     'Atomic habits',
+//     'Deep Work'
+// ]
+
+// books.forEach(function(element, index) {
+//     console.log(`${index + 1}. ${element}`)
+// });
+
+// const books = [
+//     'Clean Code',
+//     'Atomic habits',
+//     'Deep Work'
+// ]
+
+// books.map(function(element) {
+//     console.log(element.toUpperCase())
+// });
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// numbers.filter(function(element) {
+//     if (element % 2 === 0) {
+//             console.log(element)
+//         }
+//     });
+    
+// //  BELUM SELESAI
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// const genap = numbers.filter(function(element) {
+//         if (element % 2 === 0) {
+//                 return element;
+//             };
+//         });
+        
+// numbersBaru = genap.map(function(element) {
+//         return element * 10;
+//     });
+    
+//     console.log(numbersBaru);
+
+// const hasil = numbers
+//     .filter(function(element) {
+//         return element % 2 === 0;
+//     })
+//     .map(function(element) {
+//         return element * 10;
+//     });
+
+// console.log(hasil);
+            
+// //  BELUM SELESAI
+
+// const books = [
+//     {
+//         title: "Clean Code",
+//         done: true
+//     },
+//     {
+//         title: "Atomic Habits",
+//         done: false
+        
+//     },
+//     {
+//         title: "Deep Work",
+//         done: true
+//     }
+// ];
+
+// books.forEach(function(element) {
+//     if (element.done === true) {
+//         console.log(element)
+//     }
+// });
+
+// books.forEach(function(element) {
+//     console.log(element.title);
+// });
