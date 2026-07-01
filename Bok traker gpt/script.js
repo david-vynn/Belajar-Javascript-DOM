f8eb297158c1e2f7043db27957d2228d1a2c0d4e
david-vynn <davidalvinmaulana@gmail.com>
2026-07-01 09:11:58 +0700

    Belajar data methods array lagih & lagih

--- Bok traker gpt/script.js
+++ Bok traker gpt/script.js
@@ -1,78 +1,36 @@
-// Tangkap 
+// Tangkap
 
 const inputText = document.getElementById('inputText');
 const addBtn = document.getElementById('addBtn');
 
+let books = [];
+
 addBtn.addEventListener('click', function() {
+
     const inputTextValue = inputText.value;
-    const listBook = document.getElementById('listBook');
-    
+
+    let toUpper = inputTextValue.toUpperCase();
+
     if (inputTextValue.trim() === '') {
-        alert('Input tidak boleh kosong!!!');
+        alert('Gaboleh kosong lah bang!!!')
     } else {
-        const inputTextNode = document.createTextNode(inputTextValue);
 
-        let books;
+        if (books.includes(toUpper)) {
+                alert('Buku sudah dimasukkan di list');
+            } else {
+
+                books.push(inputTextValue.toUpperCase());
+
+                const inputTextNode = document.createTextNode(inputTextValue);
+                const liBaru = document.createElement('li');
+                const listBookBox = document.getElementById('listBookBox');
         
-        if (localStorage.getItem('books')) {
-            books = JSON.parse(
-                localStorage.getItem('books')
-            );
-        } else {
-            books = [];
-        }
-
-        books.push(inputTextValue);
-        localStorage.setItem('books',
-            JSON.stringify(books)
-        );
-
-
-        const liBaru = document.createElement('li');
-        const spanList = document.createElement('span');
-        const deleteBtn = document.createElement('button');
-        const textDltBtn = document.createTextNode('X');
-
-        deleteBtn.appendChild(textDltBtn);
-
-        spanList.appendChild(inputTextNode);
-        liBaru.appendChild(spanList);
-        liBaru.appendChild(deleteBtn);
-        listBook.appendChild(liBaru);
+                liBaru.appendChild(inputTextNode);
+                listBookBox.appendChild(liBaru);
                 
-        liBaru.addEventListener('click', function(e) {
-            spanList.classList.toggle('done');
-            updateDoneBook();
-        });
+                inputText.value = '';
+            }
         
-        deleteBtn.addEventListener('click', function(e) {
-            e.stopPropagation();
-            e.target.parentElement.remove();
-            
-            updateTotalBook();
-            updateDoneBook();
-        });
-
-        updateTotalBook();
     }
-
-    function updateTotalBook() {
-        const numTotBook = document.getElementById('numTotBook');
-        const listBook = document.getElementById('listBook');
-
-        const jumlahList = listBook.children.length;
-        
-        numTotBook.textContent = jumlahList;
-    };
-
-    function updateDoneBook() {
-        const numDoneBook = document.getElementById('numDoneBook');
-        const listDone = document.querySelectorAll('.done');
-        
-        const totalListDone = listDone.length;
-        
-        numDoneBook.textContent = totalListDone;
-    };
-
-    inputText.value = '';
 });
+
